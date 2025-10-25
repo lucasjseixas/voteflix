6d32b33d444204bfe026d83c3f68284a38c64a6d
Lucas <lucasjoseseixas@alunos.utfpr.edu.br>
2025-10-25 14:10:23 -0300

    commitando

--- src/voteflix/dto/response/ListarProprioUsuarioResponse.java
+++ src/voteflix/dto/response/ListarProprioUsuarioResponse.java
@@ -9,18 +9,9 @@ public class ListarProprioUsuarioResponse {
     @SerializedName("usuario")
     public String usuario;
 
-    // Construtor para sucesso
     public ListarProprioUsuarioResponse(String status, String usuario) {
         this.status = status;
         this.usuario = usuario;
     }
-
-    // Construtor para falha (usuario será null)
-    public ListarProprioUsuarioResponse(String status) {
-        this.status = status;
-    }
-
-    // Construtor padrão para GSON
     public ListarProprioUsuarioResponse() {}
 }
-
